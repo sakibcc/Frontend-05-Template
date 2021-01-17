d8a65717e881ac3b3f6b4d50304193f554b451e9
sakibcc <ud.k2007@gmail.com>
2021-01-17 16:42:55 +0800

    完成停止和恢复

--- Week 15/animation.js
+++ Week 15/animation.js
@@ -2,6 +2,8 @@ const TICK = Symbol("tick");
 const TICK_HANDLER = Symbol("tick-handler");
 const ANIMATIONS = Symbol("animations")
 const START_TIME = Symbol("start-time")
+const PAUSE_START = Symbol("paust-start")
+const PAUSE_TIME = Symbol("pause-time")
 export class TimeLine {
   constructor() {
     this[ANIMATIONS] = new Set()
@@ -9,14 +11,15 @@ export class TimeLine {
   }
   start() {
     let startTime = Date.now()
+    this[PAUSE_TIME] = 0
     this[TICK] = () => {
       let now = Date.now()
       for (let animation of this[ANIMATIONS]) {
         let t;
         if(this[START_TIME].get(animation) < startTime) {
-          t = now - startTime
+          t = now - startTime - this[PAUSE_TIME]
         } else {
-          t = now - this[START_TIME].get(animation)
+          t = now - this[START_TIME].get(animation) - this[PAUSE_TIME]
         }
         if(animation.duration < t) {
           this[ANIMATIONS].delete(animation)
@@ -25,12 +28,18 @@ export class TimeLine {
           
         animation.receive(t)
       }
-      requestAnimationFrame(this[TICK]);
+      this[TICK_HANDLER] = requestAnimationFrame(this[TICK]);
     };
     this[TICK]();
   }
-  pausel() {}
-  resume() {}
+  pausel() {
+    this[PAUSE_START] = Date.now()
+    cancelAnimationFrame(this[TICK_HANDLER])
+  }
+  resume() {
+    this[PAUSE_TIME] += Date.now() - this[PAUSE_START]
+    this[TICK]()
+  }
   reset() {}
   add(animation, startTime) {
     if(arguments.length < 2) {
@@ -49,7 +58,8 @@ export class Animation {
     endValue,
     duration,
     delay,
-    timingFunction
+    timingFunction,
+    template
   ) {
     this.object = object;
     this.property = property;
@@ -58,9 +68,10 @@ export class Animation {
     this.duration = duration;
     this.delay = delay;
     this.timingFunction = timingFunction;
+    this.template = template;
   }
   receive(time) {
     let range = this.endValue - this.startValue
-    this.object[this.property] = this.startValue + range * time / this.duration
+    this.object[this.property] = this.template(this.startValue + range * time / this.duration)
   }
 }
