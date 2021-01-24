6f8cdd723119ca1135833c9526dbae36bb828098
sakibcc <ud.k2007@gmail.com>
2021-01-24 21:34:49 +0800

    优化代码

--- Week 16/Button.js
+++ Week 16/Button.js
@@ -1,17 +1,16 @@
-import {Component,createElement} from "./framework.js";
+import { Component, createElement } from "./framework.js";
 
-export class Button extends Component{
-    constructor(){
-        super();
-    }
-    render(){
-        this.childContriner = <span/>
-        this.root = (<div>{this.childContriner}</div>).render();
-    }
+export class Button extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    this.childContriner = <span />;
+    this.root = (<div>{this.childContriner}</div>).render();
+  }
 
-    appendChild(child){
-        if(!this.childContriner)
-            this.render()
-        this.childContriner.appendChild(child);
-    }
+  appendChild(child) {
+    if (!this.childContriner) this.render();
+    this.childContriner.appendChild(child);
+  }
 }
--- Week 16/List.js
+++ Week 16/List.js
@@ -1,15 +1,15 @@
-import {Component,createElement,ATTRIBUTE} from "./framework.js";
+import { Component, createElement, ATTRIBUTE } from "./framework.js";
 
-export class List extends Component{
-    constructor(){
-        super();
-    }
-    render(){
-        this.children = this[ATTRIBUTE].data.map(this.template);
-        this.root = (<div>{this.children}</div>).render();
-    }
+export class List extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    this.children = this[ATTRIBUTE].data.map(this.template);
+    this.root = (<div>{this.children}</div>).render();
+  }
 
-    appendChild(template){
-        this.template = template;
-    }
+  appendChild(template) {
+    this.template = template;
+  }
 }
