3a9258efca6f3e940e7966435838f9aa31523e3d
yuxuanhui <saki_bc@163.com>
2021-02-19 18:25:34 +0800

    完成发布简单链路

--- Week 19/publish-server/server.js
+++ Week 19/publish-server/server.js
@@ -1,5 +1,17 @@
 let http = require('http');
+let fs = require("fs");
 http.createServer(function(req, res) {
-  console.log(req);
-  res.end("hello world")
+
+  let outFile = fs.createWriteStream("../server/public/index.html");
+  
+  req.on("data", chunk => {
+    outFile.write(chunk);
+  })
+  req.on("end", chunk => {
+    if(chunk) {
+      outFile.write(chunk);
+    }
+    outFile.end();
+    res.end("Success")
+  })
 }).listen("8082")
--- Week 19/publish-tool/publish.js
+++ Week 19/publish-tool/publish.js
@@ -1,10 +1,25 @@
 let http = require('http');
+let fs = require('fs');
 
 let request = http.request({
   hostname: "127.0.0.1",
-  port: 8082
+  port: 8082,
+  method: "POST",
+  headers: {
+    "Content-Type": "application/octet-stream"
+  }
 }, response => {
-  console.log(response)
+  // console.log(response)
 })
 
-request.end();
+let file = fs.createReadStream("./sample.html");
+
+file.on('data', chunk => {
+  console.log(chunk.toString())
+  request.write(chunk)
+})
+
+file.on('end', chunk => {
+  console.log("read finished")
+  request.end(chunk)
+})
