3a2f64c7bba18d27889849b36560b3f3d06b8a28
yuxuanhui <saki_bc@163.com>
2021-02-19 21:04:39 +0800

    增加OAUTH

--- Week 19/publish-server/server.js
+++ Week 19/publish-server/server.js
@@ -1,9 +1,74 @@
 let http = require('http');
 let unzipper = require("unzipper")
-let fs = require("fs")
-http.createServer(function(req, res) {
-  req.pipe(unzipper.Extract({path: "../server/public"}))
-  req.on("end", () => {
-    res.end("success")
+let https = require("https")
+let querystring = require("querystring")
+const {CLIENT_ID, SECRET} = require("./conf.js")
+
+function auth(request, response) {
+  let query = querystring.parse(request.url.match(/^\/auth\?([\s\S]+)$/)[1])
+  getToken(query.code, function(info) {
+    response.write(`<a href='http://localhost:8083/?token=${info.access_token}'>publish</a>`)
+    response.end()
   })
+}
+
+function getToken(code ,callback) {
+  let request = https.request({
+    hostname: "github.com",
+    path: `/login/oauth/access_token?code=${code}&client_id=${CLIENT_ID}&client_secret=${SECRET}`,
+    port: 443,
+    method: "POST"
+  }, function(response) {
+    let body = "";
+    response.on('data', chunk => {
+      body += (chunk.toString());
+    })
+    response.on('end', chunk => {
+      callback(querystring.parse(body))
+    })
+  });
+  request.end();
+}
+
+function publish(request,response) {
+  let query = querystring.parse(request.url.match(/^\/publish\?([\s\S]+)$/)[1])
+
+  getUser(query.token, info => {
+    if (info.login === 'bccdemo') {
+      request.pipe(unzipper.Extract({path: "../server/public"}))
+      request.on("end", () => {
+        response.end("success")
+      })
+    }
+  });
+}
+
+function getUser(token ,callback) {
+  let request = https.request({
+    hostname: "api.github.com",
+    path: `/user`,
+    port: 443,
+    method: "GET",
+    headers: {
+      Authorization: `token ${token}`,
+      "User-Agent": 'toy-publish'
+    }
+  }, function(response) {
+    let body = "";
+    response.on("data", chunk => {
+      body += (chunk.toString());
+    })
+    response.on("end", chunk => {
+      callback(JSON.parse(body))
+    })
+  })
+  request.end()
+}
+http.createServer(function(request, response) {
+  if(request.url.match(/^\/auth\?/)){
+    return auth(request, response)
+  } 
+  if(request.url.match(/^\/publish\?/)) {
+    return publish(request, response)
+  }
 }).listen("8082")
--- Week 19/publish-tool/publish.js
+++ Week 19/publish-tool/publish.js
@@ -1,21 +1,30 @@
 let http = require('http');
 let fs = require('fs');
 let archiver = require("archiver")
+let child_process  = require("child_process")
+let querystring = require("querystring")
+const {CLIENT_ID} = require("./conf.js")
 
-fs.stat("./simple/sample.html", (err, stats) => {
+child_process.exec(`open https://github.com/login/oauth/authorize?client_id="${CLIENT_ID}"`)
+
+http.createServer(function(request, response) {
+  let query = querystring.parse(http.request.url.match(/^\/\?([\s\S]+)$/)[1])
+  publish(query.token)
+}).listen(8083);
+
+function publish(token) {
   let request = http.request({
     hostname: "127.0.0.1",
     port: 8082,
     method: "POST",
+    path: "/publish?token=" + token,
     headers: {
       "Content-Type": "application/octet-stream",
-      // "Content-Length": stats.size
     }
   }, response => {
-    request.end()
+    console.log(response)
   })
-  
-  
+
   const archive = archiver("zip", {
     zlib: {level: 9}
   });
@@ -24,4 +33,4 @@ fs.stat("./simple/sample.html", (err, stats) => {
   archive.finalize();
 
   archive.pipe(request)
-})
+}
